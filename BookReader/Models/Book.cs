1625b5e4e599990496fbfd1298330bdc4f6bd277
Emil Kolczyński <187730@edu.p.lodz.pl>
2020-09-19 13:47:46 +0200

    Created Book Model

---
+++ BookReader/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
