608f82113cddd6841725771cbc37da3a8c62a5c2
miczuux3 <miharuu.yuni@gmail.com>
2020-09-20 19:41:16 +0200

    Logowanie

--- BookReader/Startup.cs
+++ BookReader/Startup.cs
@@ -18,6 +18,7 @@ namespace BookReader
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
