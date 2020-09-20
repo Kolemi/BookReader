0f9ec307285a82cecd09328911d38b2081f663bb
Emil Kolczyński <187730@edu.p.lodz.pl>
2020-09-20 11:31:23 +0200

    Customer model now contains address related fields

--- BookReader/Models/Customer.cs
+++ BookReader/Models/Customer.cs
@@ -12,5 +12,10 @@ namespace BookReader.Models
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
     }
 }
