e3f5698be0d279e5c17d2f0e284e1a093b5c603d
Emil Kolczyński <187730@edu.p.lodz.pl>
2020-09-20 11:41:29 +0200

    Created OrderDetails Model

---
+++ BookReader/Models/OrderDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Models
+{
+    public class OrderDetails
+    {
+        public int OrderDetailsID { get; set; }
+        public Decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public double Discount { get; set; }
+    }
+}
